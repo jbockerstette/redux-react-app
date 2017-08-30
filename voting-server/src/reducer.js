1a28559252265a942bcfcf52256276cc56e8b096
jbockerstette <jim.bockerstette@gmail.com>
2017-08-30 15:07:49 -0700

    use vote only not the whole state object.

--- voting-server/src/reducer.js
+++ voting-server/src/reducer.js
@@ -7,7 +7,7 @@ export default function reducer(state = INITIAL_STATE, action) {
     case 'NEXT':
       return next(state);
     case 'VOTE':
-      return vote(state, action.entry);
+      return state.set('vote', vote(state.get('vote'), action.entry));
     default:
       return state;
   }
